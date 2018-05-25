913c5cf400c22c741e0153cdd9d8e5ba8c40a00c
OverclockedSanic <overclockedsanic@protonmail.com>
2018-05-25 13:31:19 +0300

    Initial Solver Implementation

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from solver import Solver
 
 t1 = datetime.utcnow()
 
-# Read file
+# Read file: O(n)
 with open('wordlist_english.txt') as f:
     lines = [line.rstrip('\n') for line in f]
 
 t2 = datetime.utcnow()
 
-# Process wordlist
+# Process wordlist: O(n log n)
 wl = WordList(lines)
 
 t3 = datetime.utcnow()
 
-# Select the necessary words for the given anagram
+# Select the necessary words for the given anagram: O(n)
 bwl = BakedWordList(wl, ['anagram'])
 
 t4 = datetime.utcnow()
@@ -29,8 +29,10 @@ print('Read File:', (t2 - t1).microseconds/1000, 'ms')
 print('Phase 1:', (t3 - t2).microseconds/1000, 'ms')
 print('Phase 2:', (t4 - t3).microseconds/1000, 'ms')
 
-
 # ANAGRAMS!
 s = Solver(bwl, 2, 4)
-for anagram in s:
-    print(' '.join(anagram))
+i = 0
+for anagram in s.generator():
+    i += 1
+
+print(i)
--- solver.py
+++ solver.py
@@ -1,3 +1,5 @@
+from itertools import combinations, permutations
+
 class Solver:
 
     def __init__(self, bwordlist, minLength, maxLength):
@@ -5,71 +7,39 @@ class Solver:
         self.minLength = max(1, minLength)
         self.maxLength = maxLength
 
-    def __iter__(self):
-        self.depth = self.minLength
-        self.fingerprint_index_stack = list()
-        self.word_index_stack        = list()
-        self.charmap_left_stack      = list()
-        return self
-
-    def __next__(self):
-
-        raise NotImplementedError
-
-        #self.fingerprint_index_stack[depth-1] += 1
+    def generator(self):
+        #raise NotImplementedError
 
         # Breadth-First Search (sort of, since it's not actually searching for something in a node tree, but behaves in
         # a similar way)
-        for depth in range(self.depth, self.maxLength+1):
-            self.depth = depth
+        for depth in range(self.minLength, self.maxLength+1):
+            for subset in combinations(self.wordlist.fingerprints, depth):
 
-            # Prepare fingerprint index stack
-            self.fingerprint_index_stack = [0] * depth
+                #TODO: Check if it's a valid anagram
 
-            # Loop decrementally through stack indices
-            for f in range(depth-1, -1, -1):
+                for fingerprint_stack in permutations(subset):
 
-                if self.fingerprint_index_stack[f] < len(self.wordlist.fingerprints):
+                    # Prepare word index stack
+                    word_index_stack = [0] * depth
 
-                    # Increment the word's index so we get a different one next time
-                    self.fingerprint_index_stack[f] += 1
+                    # Loop decrementally through stack indices
+                    for i in range(depth-1, -1, -1):
 
-                else:
-                    self.fingerprint_index_stack[f] = 0
+                        # If the word index in this position is less than the length of the list of words with the same
+                        # fingerprint
+                        while word_index_stack[i] < len(self.wordlist.fingerprint_words[fingerprint_stack[i]]):
 
+                            # Get the list of words making up the anagram:
+                            #  This works by iterating through every position in the stack, getting the fingerprint from
+                            #  that position, getting the list of words with that fingerprint, selecting the word matching
+                            #  the word index from that position, adding it to the list, rinsing and repeating.
+                            result = [self.wordlist.fingerprint_words[fingerprint_stack[x]][word_index_stack[x]] for x in range(depth)]
 
+                            # Increment the word's index so we get a different one next time
+                            word_index_stack[i] += 1
 
+                            # Return
+                            yield result
 
-
-
-
-
-
-
-                # Prepare word index stack
-                self.word_index_stack = [0] * depth
-
-                # Loop decrementally through stack indices
-                for i in range(depth-1, -1, -1):
-
-                    # If the word index in this position is less than the length of the list of words with the same
-                    # fingerprint
-                    if self.word_index_stack[i] < len( self.wordlist.fingerprint_words[self.wordlist.fingerprints[self.fingerprint_index_stack[i]]] ):
-
-                        # Get the list of words making up the anagram:
-                        #  This works by iterating through every position in the stack, getting the fingerprint from
-                        #  that position, getting the list of words with that fingerprint, selecting the word matching
-                        #  the word index from that position, adding it to the list, rinsing and repeating.
-                        result = [self.wordlist.fingerprint_words[self.wordlist.fingerprints[self.fingerprint_index_stack[x]]][self.word_index_stack[x]] for x in range(depth)]
-
-                        # Increment the word's index so we get a different one next time
-                        self.word_index_stack[i] += 1
-
-                        # Return
-                        return result
-
-                    # Otherwise, set this word's index to 0 and try to increment the one before it
-                    else:
-                        self.word_index_stack[i] = 0
-
-        raise StopIteration
+                        # Set this word's index to 0 and try to increment the one before it
+                        word_index_stack[i] = 0
